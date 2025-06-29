d2a7531bed248d4903500a0952f49e0b57c9c077
Dylan Castillo <dylanjcastillo@gmail.com>
2025-06-29 19:23:30 +0200

    Update to Kamal 2

--- src/config/settings.py
+++ src/config/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "config.middleware.HealthCheckMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
